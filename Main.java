fd321532ea7d8bae664f1f410e44d9ccb2211314
winston-da-best <kazoos.dearies-0t@icloud.com>
2025-11-14 22:21:34 -0500

    Real code fr

--- Main.java
+++ Main.java
@@ -1,3 +1,14 @@
 public class Main {
-  /*HIIIIIIIII */
+
+   public static void main(String[] args ){
+    int numDogs = 5;
+    String nameOfUser = "Billy Bob Johnson 67 ";
+    double rectWidth = 6.67;
+    double rectHeight = 7.66667;
+    char firstLetter = 'B';
+    Boolean doorIsOpen = false; 
+    
+   }
+  
+
 }
